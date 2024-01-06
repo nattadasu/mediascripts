0afe72858262bd78ed9ecc8959ba77315ca15976
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-01-06 18:16:33 +0700

    Improve lrc embedder

--- lrcembedder/lrcembedder.py
+++ lrcembedder/lrcembedder.py
@@ -4,15 +4,92 @@ import re
 import io
 from enum import Enum
 from dataclasses import dataclass
+from datetime import datetime
 
 from mutagen.mp4 import MP4
-from mutagen.mp3 import MP3
+from mutagen.id3 import ID3, USLT, SYLT
 from mutagen.flac import FLAC
 
 
+# set log file path on current working directory
+now = datetime.now()
+log_path = os.path.join(os.getcwd(), f'lrcembedder_{now.strftime("%Y%m%d_%H%M%S")}.log')
 mp4_exts = ('.m4a', '.m4b', '.m4p', '.m4r', '.mp4', '.aac', '.alac')
 timestamp_pattern = re.compile(r'\[(\d{2}):(\d{2})\.(\d{2,3})\]')
 
+
+def colorize(text: str, color: str = 'white', bg: str = 'black', bold: bool = False) -> str:
+    """
+    Colorize text
+
+    :param text: text to colorize
+    :type text: str
+    :param color: text color, defaults to 'white'
+    :type color: str, optional
+    :param bg: background color, defaults to 'black'
+    :type bg: str, optional
+    :param bold: whether to bold the text, defaults to False
+    :type bold: bool, optional
+    :return: colorized text
+    :rtype: str
+    """
+    colors = {
+        'black': 30,
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'blue': 34,
+        'purple': 35,
+        'cyan': 36,
+        'white': 37
+    }
+    bgs = {
+        'black': 40,
+        'red': 41,
+        'green': 42,
+        'yellow': 43,
+        'blue': 44,
+        'purple': 45,
+        'cyan': 46,
+        'white': 47
+    }
+    color_code = colors.get(color, 37)
+    bg_code = bgs.get(bg, 40)
+    bold_code = 1 if bold else 0
+    return f'\033[{bold_code};{color_code};{bg_code}m{text}\033[0m'
+
+def uncolorize(text: str) -> str:
+    """
+    Remove color from text
+
+    :param text: text to remove color
+    :type text: str
+    :return: uncolorized text
+    :rtype: str
+    """
+    return re.sub(r'\033\[\d+(?:;\d+)*m', '', text)
+
+def reprint(text: str = "", log_path: str = log_path):
+    """
+    Print text to stdout and log file
+
+    :param text: text to print
+    :type text: str
+    :param log_path: log file path
+    :type log_path: str
+    """
+    if len(text) > 0:
+        now = datetime.now()
+        date = colorize(now.strftime("%Y-%m-%d"), color='blue')
+        time = colorize(now.strftime("%H:%M:%S"), color='green')
+        text = f'[{date} {time}] {text}'
+    sys.stdout.write(text + '\n')
+    if len(text) == 0:
+        return
+    with open(log_path, 'a') as file:
+        file.write(uncolorize(text) + '\n')
+
+print = reprint
 
 class SyncStatus(Enum):
     UNSYNCED = 0
@@ -54,7 +131,7 @@ class Timestamp:
             ms = int(milliseconds)
             return Timestamp(h, m, s, ms)
         return Timestamp()
-    
+
     @staticmethod
     def from_milliseconds(milliseconds: int) -> 'Timestamp':
         """
@@ -111,6 +188,39 @@ def check_sync_or_unsync(path_or_content: str, is_content: bool = False) -> Sync
     return SyncStatus.UNSYNCED
 
 
+class LyricComparisonResult(Enum):
+    ERROR = -1
+    EQUAL = 0
+    FILE_IS_SYNC = 1
+    EMBEDDED_IS_SYNC = 2
+
+def compare_lrc_to_embedded_lrc(lrc_path: str, embedded_lrc: str) -> LyricComparisonResult:
+    """
+    Compare .lrc file to embedded lyrics
+
+    :param lrc_path: .lrc file path
+    :type lrc_path: str
+    :param embedded_lrc: embedded lyrics
+    :type embedded_lrc: str
+    :return: comparison result
+    :rtype: LyricComparisonResult
+    """
+    file_sync_status = check_sync_or_unsync(lrc_path)
+    embedded_sync_status = check_sync_or_unsync(embedded_lrc, is_content=True)
+
+    if file_sync_status is None or embedded_sync_status is None:
+        return LyricComparisonResult.ERROR
+
+    if file_sync_status == embedded_sync_status:
+        return LyricComparisonResult.EQUAL
+    elif file_sync_status == SyncStatus.SYNCED:
+        return LyricComparisonResult.FILE_IS_SYNC
+    elif embedded_sync_status == SyncStatus.SYNCED:
+        return LyricComparisonResult.EMBEDDED_IS_SYNC
+    else:
+        return LyricComparisonResult.EQUAL
+
+
 def destruct_lyrics(content: str) -> list[Lyric]:
     """
     Destruct lyrics to list of Lyric objects
@@ -148,19 +258,38 @@ def construct_lyrics(lyrics: list[Lyric]) -> str:
     return content
 
 
-def fix_lyrics(text: str) -> str:
+def construct_sylt(lyrics: list[Lyric]) -> SYLT:
+    """
+    Construct SYLT lyrics from list of Lyric objects
+
+    :param lyrics: list of Lyric objects
+    :type lyrics: list[Lyric]
+    :return: SYLT object
+    :rtype: SYLT
+    """
+
+    tuples: list[tuple[str, int]] = []
+
+    for lyric in lyrics:
+        tuples.append((lyric.text, lyric.timestamp.to_milliseconds()))
+
+    return SYLT(encoding=3, lang='eng', desc='', text=tuples)
+
+def fix_lyrics(text: str, use_cr: bool = True) -> str:
     """
     Fix lyrics text to be properly formatted
 
     :param text: lyrics text
     :type text: str
+    :param use_cr: whether to use CRLF instead of LF
+    :type use_cr: bool, defaults to True
     :return: fixed lyrics text
     :rtype: str
     """
     # if its BOM, remove it
     if text.startswith('\ufeff'):
         text = text[1:]
-    if '\r' not in text:
+    if '\r' not in text and use_cr:
         # Replace LF with CRLF
         text = text.replace('\n', '\r\n')
 
@@ -173,6 +302,7 @@ def fix_lyrics(text: str) -> str:
         # print first 3 lyrics
         for lyric in lyrics[:3]:
             print(f"      {lyric}")
+        print('      ...') if len(lyrics) > 3 else None
         # if the first timestamp is not 0, add a timestamp at the beginning
         print(f"    First timestamp: {lyrics[0].timestamp}")
         milli = lyrics[0].timestamp.to_milliseconds()
@@ -182,6 +312,7 @@ def fix_lyrics(text: str) -> str:
             print("    Added a timestamp at the beginning, new first 3 lines:")
             for lyric in lyrics[:3]:
                 print(f"      {lyric}")
+            print('      ...') if len(lyrics) > 3 else None
         text = construct_lyrics(lyrics)
     # clear empty lines
     text = re.sub(r'((?:\r\n)+\r\n)', r'\r\n', text)
@@ -189,7 +320,7 @@ def fix_lyrics(text: str) -> str:
     return text
 
 
-def export_lyrics_to_file(path: str, fmt: str = "lrc", force_overwrite: bool = False) -> bool:
+def export_lyrics_to_file(path: str, fmt: str = "lrc", force_overwrite: bool = False, compat: bool = True) -> bool:
     """
     Export lyrics to file
 
@@ -209,21 +340,27 @@ def export_lyrics_to_file(path: str, fmt: str = "lrc", force_overwrite: bool = F
     synced_lyrics = None
     unsynced_lyrics = None
     if path.endswith('.mp3'):
-        audio = MP3(path)
-        # find first dict with key that stsarts with 'USLT::'
-        for key in audio.keys():
-            if key.startswith('USLT::'):
-                unsynced_lyrics = audio[key].text
-                synced_lyrics = unsynced_lyrics
-                break
-        lyrics = []
-        for key in audio.keys():
-            if key.startswith('SYLT::'):
-                synced_lyrics = audio[key].text
-                # construct tuple of (text, timestamp) to list of Lyric objects
+        audio = ID3(path)
+        unsynced_lyrics = audio.getall('USLT')
+        if unsynced_lyrics and len(unsynced_lyrics) > 1:
+            print(f'  Export: Multiple USLT tags found in {path}, only the first one will be used')
+        synced_lyrics = audio.getall('SYLT')
+        if synced_lyrics and len(synced_lyrics) > 1:
+            print(f'  Export: Multiple SYLT tags found in {path}, only the first one will be used')
+        # Use first item in list
+        if unsynced_lyrics:
+            unsynced_lyrics = unsynced_lyrics[0].text
+        try:
+            if synced_lyrics:
+                synced_lyrics = synced_lyrics[0].text
                 lyrics = [Lyric(Timestamp.from_milliseconds(line[1]), line[0]) for line in synced_lyrics]
                 synced_lyrics = construct_lyrics(lyrics)
-                break
+            else:
+                synced_lyrics = unsynced_lyrics[0].text
+        except Exception as e:
+            print(f'  Export: Failed to read synced lyrics from {path}: {e}')
+            print("    Reusing unsynced lyrics")
+            synced_lyrics = unsynced_lyrics
     elif path.endswith(mp4_exts):
         audio = MP4(path)
         unsynced_lyrics = audio.get('\xa9lyr', None)
@@ -238,7 +375,7 @@ def export_lyrics_to_file(path: str, fmt: str = "lrc", force_overwrite: bool = F
 
     if not unsynced_lyrics and not synced_lyrics:
         print(f'  Export: No lyrics found in {path}')
-        if type(audio) is MP3:
+        if type(audio) is ID3 and (len(audio.getall('USLT')) > 0 or len(audio.getall('SYLT')) > 0):
             print("    There's possibly a SYLT/USLT tag in the file, but it seems the logic to read lyric skipped it accidentally")
             print("    Please report this issue to the developer")
         return False
@@ -249,19 +386,25 @@ def export_lyrics_to_file(path: str, fmt: str = "lrc", force_overwrite: bool = F
         return False
 
     with open(target_path, 'w') as file:
+        check_sync = check_sync_or_unsync(target_path, synced_lyrics or unsynced_lyrics)
         if synced_lyrics:
             lyrics = synced_lyrics[0] if type(synced_lyrics) is list else synced_lyrics
-            fixed_lyrics = fix_lyrics(lyrics)
-            file.write(fixed_lyrics)
+            fixed_lyrics = fix_lyrics(lyrics, use_cr=compat)
+            match check_sync:
+                case LyricComparisonResult.FILE_IS_SYNC:
+                    print("  Export: .lrc is synced compared to embedded lyrics, skip to avoid data loss (even on force overwrite)")
+                    return False
+                case _:
+                    file.write(fixed_lyrics)
         else:
             lyrics = unsynced_lyrics[0]
-            fixed_lyrics = fix_lyrics(lyrics)
+            fixed_lyrics = fix_lyrics(lyrics, use_cr=compat)
             file.write(fixed_lyrics)
 
     return True
 
 
-def import_lyrics_from_file(path: str) -> bool:
+def import_lyrics_from_file(path: str, compat: bool = True) -> bool:
     """
     Import lyrics from file
 
@@ -276,7 +419,7 @@ def import_lyrics_from_file(path: str) -> bool:
 
     audio = None
     if path.endswith('.mp3'):
-        audio = MP3(path)
+        audio = ID3(path)
     elif path.endswith(mp4_exts):
         audio = MP4(path)
     elif path.endswith('.flac'):
@@ -306,26 +449,21 @@ def import_lyrics_from_file(path: str) -> bool:
         lyrics = file.read()
 
     # fix lyrics
-    lyrics = fix_lyrics(lyrics)
+    lyrics = fix_lyrics(lyrics, use_cr=compat)
 
     # write lyrics to audio file
-    if type(audio) is MP3:
-        # with open(f"{path}.mutagen.pydump", "w") as f:
-        #     print(f"    Dumping audio file to {path}.mutagen.pydump...")
-        #     f.write(repr(audio))
-        if status == SyncStatus.SYNCED:
-            # find first dict with key that stsarts with 'USLT::'
-            destruct = destruct_lyrics(lyrics)
-            # convert to list of tuple (text, timestamp)
-            text_timestamp = [(lyric.text, lyric.timestamp.to_milliseconds()) for lyric in destruct]
-            for key in audio.keys():
-                if key.startswith('SYLT::'):
-                    audio[key].text = text_timestamp
-                    break
-        for key in audio.keys():
-            if key.startswith('USLT::'):
-                audio[key].text = lyrics
-                break
+    if type(audio) is ID3:
+        # delete existing USLT and SYLT tags
+        try:
+            audio.delall('USLT')
+        except KeyError:
+            pass
+        try:
+            audio.delall('SYLT')
+        except KeyError:
+            pass
+        audio['USLT'] = USLT(encoding=3, lang='eng', desc='', text=lyrics)
+        audio['SYLT'] = construct_sylt(destruct_lyrics(lyrics))
     elif type(audio) is MP4:
         audio['\xa9lyr'] = lyrics
     elif type(audio) is FLAC:
@@ -338,7 +476,7 @@ def import_lyrics_from_file(path: str) -> bool:
     return True
 
 
-def main(user_input: str | None = None, overwrite: bool | None = None, loop: int = 0) -> dict[str, int]:
+def main(user_input: str | None = None, overwrite: bool | None = None, loop: int = 0, windows_compat: bool | None = None) -> dict[str, int]:
     if loop == 0:
         user_input = user_input or input('Enter folder path: ')
         if not os.path.exists(user_input):
@@ -349,10 +487,15 @@ def main(user_input: str | None = None, overwrite: bool | None = None, loop: int
             overwrite = True
         else:
             overwrite = False
+        windows_compat = input('Use Windows CLRF instead of *nix LF? Note for Musicbee user, CRLF will show as 2 separate lines (y/N): ')
+        if windows_compat is True or (type(windows_compat) is str and windows_compat.lower() == 'y'):
+            windows_compat = True
+        else:
+            windows_compat = False
     else:
         buff = io.StringIO()
         sys.stdout = buff
-    
+
     expected, total = 0, 0
     fmt: dict[str, int] = {}
 
@@ -364,9 +507,9 @@ def main(user_input: str | None = None, overwrite: bool | None = None, loop: int
                 try:
                     path = os.path.join(root, file)
                     print(f'Processing {path}...')
-                    exp_status = export_lyrics_to_file(path, force_overwrite=overwrite)
+                    exp_status = export_lyrics_to_file(path, force_overwrite=overwrite, compat=windows_compat)
                     print("  Exported lyrics to file") if exp_status else print("  Skipped exporting lyrics")
-                    imp_status = import_lyrics_from_file(path)
+                    imp_status = import_lyrics_from_file(path, compat=windows_compat)
                     print("  Imported lyrics from file") if imp_status else print("  Import failed")
                     if exp_status or imp_status:
                         total += 1
@@ -381,7 +524,7 @@ def main(user_input: str | None = None, overwrite: bool | None = None, loop: int
                     with open(path, 'r') as f:
                         content = f.read()
                     print('  Trying to fix the lyric file')
-                    fixed = fix_lyrics(content)
+                    fixed = fix_lyrics(content, use_cr=windows_compat)
                     print("  Function run successfully")
                     with open(path, 'w') as f:
                         f.write(fixed)
@@ -391,12 +534,15 @@ def main(user_input: str | None = None, overwrite: bool | None = None, loop: int
                     print(f'  Failed to fix {file}: {e}')
                     sys.exit(1)
                 print()
+            else:
+                print(f'Skipped {file} (not supported file format)')
+                print()
 
     if loop == 1:
         return fmt
 
     print("Do final check... This might take a while")
-    fmt = main(user_input=user_input, overwrite=overwrite, loop=1)
+    fmt = main(user_input=user_input, overwrite=overwrite, loop=1, windows_compat=windows_compat)
     sys.stdout = sys.__stdout__
     print("Final formatting done\n")
 
