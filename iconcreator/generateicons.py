b10403fdfbb970371c5a9d2c27f1736118735fa9
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-01-10 04:31:53 +0700

    Translate and merge logics from fixthumbnail.psm1

--- iconcreator/generateicons.py
+++ iconcreator/generateicons.py
@@ -1,6 +1,8 @@
 import subprocess
 import sys
+from platform import system
 import os
+from math import ceil
 
 # Replace if necessary
 magick_path: str = os.path.join("C:\\", "Program Files", "ImageMagick-7.1.1-Q16-HDRI", "magick.exe")
@@ -16,13 +18,14 @@ def convert_jpg_to_png(path: str) -> str:
     return new_path
 
 
-def convert_png_to_ico(path: str, gravity: str = "center", transparent: str = "white",
-                       resize: int = 256, background: str = "transparent") -> str:
+def convert_png_to_ico(path: str, gravity: str = "center", resize: int = 256,
+                       background: str = "transparent") -> str:
     """Converts a png image to an ico"""
     new_path = path.replace(".png", ".ico")
     subprocess.run([magick_path, "convert", path, "-gravity", gravity, "-resize",
                     f"{resize}x{resize}", "-background", background, "-extent", f"{resize}x{resize}",
-                    "-transparent", transparent, new_path])
+                    new_path])
+    return new_path
     return new_path
 
 def delete_png(path: str, png_generated: str | bool = False) -> None:
@@ -42,6 +45,29 @@ FolderType=Pictures
 IconResource={folder_icon},0
 """)
 
+
+def set_windows_folder_icon(*path: str) -> None:
+    """Sets a folder icon for Windows"""
+    folder_icon = "poster.ico" if os.path.exists(os.path.join(*path, "poster.ico")) else "folder.ico"
+    subprocess.run(["attrib", "+r", os.path.join(*path)])
+    subprocess.run(["attrib", "+h", os.path.join(*path, "desktop.ini")])
+    subprocess.run(["attrib", "+r", os.path.join(*path, folder_icon)])
+
+
+def refresh_folder(*path: str) -> None:
+    """Refreshes a folder"""
+    from win32com.shell import shellcon, shell # type: ignore
+    shell.SHChangeNotify(shellcon.SHCNE_ASSOCCHANGED, shellcon.SHCNF_IDLIST, None, None)
+
+def configure_folder_icon(*path: str) -> None:
+    """Configures folder icon for all platforms"""
+    match system():
+        case "Windows":
+            set_windows_folder_icon(*path)
+        case _:
+            print("Unsupported OS")
+            sys.exit(1)
+
 def do_stuff(*paths: str) -> None:
     """Converts folder containing jpg images to ico"""
     print("Converting poster on " + '\\'.join(paths))
@@ -56,23 +82,56 @@ def do_stuff(*paths: str) -> None:
 
     # convert PNG to ICO
     for path, png_generated in image_paths:
-        # uncomment if you want to get asked everytime
-        # do_it = input(f"Convert {path} to ICO? (y/n): ")
-        # if do_it.lower() != "y":
-        #     # delete png if generated
-        #     delete_png(path, png_generated)
-        #     continue
         convert_png_to_ico(path)
         delete_png(path, png_generated)
         dump_desktop_ini(*paths)
+        configure_folder_icon(*paths)
         print(f"Converted {path} to ICO")
         return
     else:
         print("No images found")
-    
-if __name__ == "__main__":
-    load_path = input("Enter path to folder containing poster and folder images: ")
-    # recursively fetch directories in load_path
-    for root, dirs, files in os.walk(load_path):
+
+def yeet(path: str) -> None:
+    """Run the process"""
+    for root, dirs, files in os.walk(path):
         for dir in dirs:
             do_stuff(root, dir)
+            print("=" * os.get_terminal_size().columns)
+    if system() == "Windows":
+        print("Refreshing folder")
+        refresh_folder(path)
+    print("Done!")
+
+
+def remove_file_attribs(root_: str, dir_: str, target: str | None = None) -> None:
+    """Removes file attributes"""
+    path = os.path.join(root_, dir_, target) if target else os.path.join(root_, dir_)
+    if target:
+        subprocess.run(["attrib", "-r", "-s", "-h", path])
+    else:
+        subprocess.run(["attrib", "-r", "-s", "-h", path, "/d", "/s"])
+
+if __name__ == "__main__":
+    load_path = input("Enter path to folder containing poster and folder images: ")
+    try:
+        yeet(load_path)
+    except KeyboardInterrupt:
+        print("Exiting...")
+        sys.exit(0)
+    except PermissionError:
+        print("=" * os.get_terminal_size().columns)
+        print("Permission denied, clearing attributes if necessary")
+        for root, dirs, files in os.walk(load_path):
+            for dir in dirs:
+                curr = os.path.join(root, dir)
+                print(f"Clearing attributes on {curr}")
+                remove_file_attribs(root, dir)
+                if os.path.exists(os.path.join(root, dir, "desktop.ini")):
+                    remove_file_attribs(root, dir, "desktop.ini")
+                if os.path.exists(os.path.join(root, dir, "poster.ico")):
+                    remove_file_attribs(root, dir, "poster.ico")
+                if os.path.exists(os.path.join(root, dir, "folder.ico")):
+                    remove_file_attribs(root, dir, "folder.ico")
+                print("-" * ceil((os.get_terminal_size().columns)/2))
+        print("Retrying...")
+        yeet(load_path)
