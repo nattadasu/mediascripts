02db067cd2d44e5d7154f3702f02256cd049b21e
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-01-10 04:32:59 +0700

    Drop fixthumbnail.psm1

--- iconcreator/refresh_shell.cs
+++
@@ -1,8 +0,0 @@
-// Following C# code is used to refresh the folder icons after the icon has been changed.
-// Code is taken from https://stackoverflow.com/a/49818607/13292223
-[System.Runtime.InteropServices.DllImport("Shell32.dll")]
-private static extern int SHChangeNotify(int eventId, int flags, IntPtr item1, IntPtr item2);
-
-public static void Refresh() {
-    SHChangeNotify(0x8000000, 0x1000, IntPtr.Zero, IntPtr.Zero);
-}
